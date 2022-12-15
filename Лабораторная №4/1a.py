b6271ebee728fac0d44d0e61bd9498647cccee19
goldobster <106154546+goldobster@users.noreply.github.com>
2022-12-15 13:31:30 +0300

    Add files via upload

---
+++ Лабораторная №4/1a.py
@@ -0,0 +1,8 @@
+import math
+x = float(input('Введите x:'))
+y = float(input('Введите y:'))
+z = float(input('Введите z:'))
+if z==(math.sin(x)**2 + math.cos(y)**2)*2:
+    print('yes')
+else:
+    print('no')
